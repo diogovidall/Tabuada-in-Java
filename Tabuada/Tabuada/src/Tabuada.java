ba06e7a346d65b8d8ad01755877575bdc2577f60
Diogo Vidal <111745761+diogovidall@users.noreply.github.com>
2022-12-10 19:59:33 -0300

    Add files via upload

---
+++ Tabuada/Tabuada/src/Tabuada.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Tabuada {
+    public static void main(String[] args) {
+        int valor;
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor da tabuada:");
+        valor=teclado.nextInt();
+        for (int i = 0; i <=10; i++) {
+            System.out.println(valor + " x " + i + " = " + (valor*i));
+        }
+    }
+}
